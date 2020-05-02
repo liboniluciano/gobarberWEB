edcb44cffd4530bceb1ff161eca3527dac6ff0a8
Luciano Liboni <luciano_liboni@hotmail.com>
2020-05-01 23:57:54 -0300

    Foto do perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,8 @@ import { Form, Input } from '@rocketseat/unform';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import AvatarInput from './AvatarInput';
+
 import { Container } from './styles';
 
 export default function Profile() {
@@ -17,8 +19,10 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome completo" />
-        <Input name="email" type="email" placeholder="Seu endereço de email" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
         <hr />
 
@@ -27,14 +31,16 @@ export default function Profile() {
           name="oldPassword"
           placeholder="Sua senha atual"
         />
-        <Input type="password" name="password" placeholder="Sua senha nova" />
+        <Input type="password" name="password" placeholder="Nova senha" />
         <Input
           type="password"
           name="confirmPassword"
           placeholder="Confirmação de senha"
         />
+
         <button type="submit">Atualizar perfil</button>
       </Form>
+
       <button type="button">Sair do GoBarber</button>
     </Container>
   );
